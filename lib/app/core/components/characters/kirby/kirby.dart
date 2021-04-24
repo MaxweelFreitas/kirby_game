2cb6596b151bce5e9b01c52f79396fa19e8bd5d8
Maxweel Freitas <maxweel.robot@outlook.com>
2021-04-24 19:31:10 -0300

    assets

--- lib/app/core/components/characters/kirby/kirby.dart
+++ lib/app/core/components/characters/kirby/kirby.dart
@@ -3,6 +3,6 @@ import 'package:flutter/material.dart';
 class Kirby extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Image.asset('assets/sprites/kong/kong.png', height: 60);
+    return Image.asset('assets/sprites/kirby/stoped.png', height: 60);
   }
 }
